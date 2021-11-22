918a40d693594755acb4043374b02120eb4e45ff
Luke Fowler <luciusfow@gmail.com>
2021-11-22 11:22:28 -0500

    commit tree 06c3f784d77159b688c06e380dba52b3b5bc6cb2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, gift){
+    let thankYous = []
+    for(let i = 0; i <names.length; i++ ) {
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${gift} gift!`)
+    }
+    return thankYous
+}
+
+function countDown(num){
+    while (num > 0) {
+        console.log(num)
+        num -=1
+    }
+    console.log( num)
+}
